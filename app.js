1ea2b4569ad12f6bfe93919f5df344b73f14bc84
akheel <akheelkm007@gmail.com>
2016-03-20 10:16:17 +0530

    Optimise for upload to openShift

--- app.js
+++ app.js
@@ -8,7 +8,37 @@ var bodyParser = require('body-parser');
 //Database Access
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/userList');
+
+
+
+
+
+
+
+
+
+var db_name = 'userList';
+var mongodb_connection_string;
+
+//provide a sensible default for local development
+mongodb_connection_string = 'mongodb://127.0.0.1:27017/' + db_name;
+//take advantage of openshift env vars when available:
+if(process.env.OPENSHIFT_MONGODB_DB_URL){
+  mongodb_connection_string = process.env.OPENSHIFT_MONGODB_DB_URL + db_name;
+}
+
+
+var db = monk(mongodb_connection_string);
+
+
+
+
+
+
+
+
+
+
 
 //Session Management
 var sessions = require("client-sessions");
